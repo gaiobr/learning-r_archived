3b2c08d48b4883c9321c4bd77d8628b6f394e236
gaiobr <jhonguedes@gmail.com>
2020-06-08 07:40:53 -0300

    timetk functions added - visualize time series

--- r4ds/scripts/playing.R
+++ r4ds/scripts/playing.R
@@ -27,3 +27,51 @@ vecs <- list(
   c(NA, NA, 3, 4, 5)
 )
 coalesce(!!!vecs)
+
+
+# case_when ---------------------------------------------------------------
+# https://dplyr.tidyverse.org/reference/case_when.html
+
+library(dplyr)
+
+data(mtcars)
+head(mtcars)
+
+mtcars %>% 
+  mutate(carb2 = case_when(
+    carb > 5 | carb <= 2 ~ 'others',
+    carb == 3 ~ 'three',
+    carb == 4 ~ 'four'
+  ))
+
+
+# timetk ------------------------------------------------------------------
+# https://www.business-science.io/code-tools/2020/06/05/timetk-vesion-2-announcement.html 
+library(tidyverse)
+library(lubridate)
+library(timetk)
+
+# Static ggplot
+taylor_30_min %>%
+  plot_time_series(date, value, .color_var = week(date),
+                   .interactive = FALSE, .color_lab = "Week")
+
+# INTERACTIVE Plotly
+taylor_30_min %>%
+  plot_time_series(date, value, .color_var = week(date),
+                   .interactive = TRUE, .plotly_slider = TRUE, .color_lab = "Week")
+
+# Visualize anomalies
+walmart_sales_weekly %>%
+  group_by(Store, Dept) %>%
+  plot_anomaly_diagnostics(Date, Weekly_Sales, 
+                           .facet_ncol = 3, .interactive = FALSE)
+
+# Make a seasonality plot
+taylor_30_min %>%
+  plot_seasonal_diagnostics(date, value, .interactive = FALSE)
+
+# Inspect autocorrelation, partial autocorrelation (and cross correlations too)
+taylor_30_min %>%
+  plot_acf_diagnostics(date, value, .lags = "1 week", .interactive = FALSE)
+
